d6d8704f0062070e07ab614175c5b38dfc861488
Incredibletochi <victor.oraekwuotu@gmail.com>
2023-02-08 13:41:31 +0000

    Task 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""
-Summarize an employee's TODO list and write it to a file as JSON
-"""
-from argparse import ArgumentParser
-from json import dump
-from os import path
-from requests import get
-from sys import 
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
+import requests
+import sys
 
-USERS = 'https://jsonplaceholder.typicode.com/users'
-TODOS = 'https://jsonplaceholder.typicode.com/todos'
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-if __name__ == '__main__':
-    parser = ArgumentParser(prog=path.basename(argv[0]))
-    parser.add_argument('id', type=int, help='employee ID')
-    args = parser.parse_args()
-    user = get('/'.join([USERS, str(args.id)])).json()
-    with open('.'.join([str(args.id), 'json']), 'w') as ostream:
-        dump({
-            str(args.id): [{
-                "task": task['title'],
-                "completed": task['completed'],
-                "username": user['username'],
-            } for task in get(TODOS, params={'userId': args.id}).json()]
-        }, ostream)
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
